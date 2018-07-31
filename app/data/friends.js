d2b20c1a45fd8fd704fdfec6cd6a04e120915348
Hazar <arif.aliyev@gmail.com>
2018-07-30 23:33:32 -0500

    Homework

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Jeff Smith',
+      profilePic: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: 'Emily Johns',
+      profilePic: 'https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+      scores: [
+        "3",
+        "4",
+        "2",
+        "3",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: 'Emma Stone',
+      profilePic: 'https://images.pexels.com/photos/33111/author-jewellery-lipstick-eyelashes.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      scores: [
+        "2",
+        "2",
+        "3",
+        "4",
+        "3",
+        "2",
+        "1",
+        "3",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: 'Olivia Hill',
+      profilePic: 'https://images.pexels.com/photos/33111/author-jewellery-lipstick-eyelashes.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      scores: [
+        "1",
+        "4",
+        "5",
+        "2",
+        "5",
+        "4",
+        "3",
+        "5",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: 'Tom Brown',
+      profilePic: 'https://images.pexels.com/photos/325682/pexels-photo-325682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      scores: [
+        "3",
+        "2",
+        "3",
+        "4",
+        "3",
+        "2",
+        "2",
+        "3",
+        "3",
+        "1"
+      ]
+    },
+    {
+      name: 'Tom Williams',
+      profilePic: 'https://images.pexels.com/photos/21278/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      scores: [
+        "3",
+        "2",
+        "3",
+        "4",
+        "3",
+        "2",
+        "2",
+        "3",
+        "3",
+        "1"
+      ]
+    },
+    
+
+  ]
+  
+  module.exports = friendList;
